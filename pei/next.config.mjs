a0a49d7d45d0b607a3b4ffbd55139c543507e1cf
guiprei <guilhermelgsilva@hotmail.com>
2024-06-19 05:38:53 -0300

    second

--- pei/next.config.mjs
+++ pei/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/seguranca-publica",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
 
 export default nextConfig;
