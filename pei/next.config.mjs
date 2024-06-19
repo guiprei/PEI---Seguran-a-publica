0e1ea610008cf8662b9caa2c7d0ad11853e12203
guiprei <guilhermelgsilva@hotmail.com>
2024-06-19 05:57:23 -0300

    resolvido

--- pei/next.config.mjs
+++ pei/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/seguranca-publica",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
 };
 
 export default nextConfig;
